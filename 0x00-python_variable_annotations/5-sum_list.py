6bde809f9ec67cf18466c9568d298530e1237cd2
MoodyEG <moodyeg2011@yahoo.com>
2024-08-01 15:27:50 +0300

    Testing 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ Sum list """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Sum list """
     sum:float = 0
     for x in input_list:
         sum += x
     return sum
+print(sum_list.__annotations__)
