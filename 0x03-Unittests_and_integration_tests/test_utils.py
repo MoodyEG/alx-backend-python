979bd2dd1cd9cbccaeb7497d32a30eca144f18ea
MoodyEG <moodyeg2011@yahoo.com>
2024-08-26 14:11:54 +0300

    Testing 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,21 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(
-        self, nested_map: Mapping[str, Any], path: Sequence[str], expected: Any
+        self, nested_map: Mapping[str, Any],
+        path: Sequence[str], expected: Any
     ) -> None:
-        """ Test access_nested_map function """
+        """ Test access_nested_map function, normally """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        (dict(), ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+    ])
+    def test_access_nested_map_exception(
+        self, nested_map: Mapping[str, Any],
+        path: Sequence[str], expected_msg: str
+    ) -> None:
+        """ Test access_nested_map function, with raise """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), expected_msg)
