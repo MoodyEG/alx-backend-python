81bbd2fba987039951ceef246f11879c112221ea
MoodyEG <moodyeg2011@yahoo.com>
2024-08-26 16:49:07 +0300

    Testing 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Testing client"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class"""
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    @patch('client.get_json', return_value={"payload": True})
+    def test_org(
+        self, org_name: str, mock_get_json: unittest.mock.MagicMock
+    ) -> None:
+        """Test that GithubOrgClient.org returns the correct value"""
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, {"payload": True})
+        mock_get_json.assert_called_once_with(
+            GithubOrgClient.ORG_URL.format(org=org_name)
+        )
